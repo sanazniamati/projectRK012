f915643bf8fc2c126c2c1c09911b528466b258f5
sanazniamati <sanaz.niamati1991@gmail.com>
2022-06-21 20:45:15 +0430

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Stage, Layer, RegularPolygon } from "react-konva";
+const App = () => {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <RegularPolygon
+          x={150}
+          y={150}
+          sides={6}
+          radius={70}
+          fill={"red"}
+          stroke={"black"}
+          strokeWidth={4}
+        />
+      </Layer>
+    </Stage>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
